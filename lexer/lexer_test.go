940747243d0256b3b7b36e7058349876051c3311
Bo Kleynen <kleynenbo@gmail.com>
2018-10-06 15:21:58 +0200

    Chapter 2 - Parser

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,16 +1,17 @@
 package lexer
 
 import (
-	"monkey/token"
 	"testing"
+
+	"monkey/token"
 )
 
-func TestNextToke(t *testing.T) {
+func TestNextToken(t *testing.T) {
 	input := `let five = 5;
 let ten = 10;
 
 let add = fn(x, y) {
-	x + y;
+  x + y;
 };
 
 let result = add(five, ten);
@@ -26,6 +27,7 @@ if (5 < 10) {
 10 == 10;
 10 != 9;
 `
+
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
@@ -112,11 +114,13 @@ if (5 < 10) {
 		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
 		}
 
 		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
 		}
 	}
 }
