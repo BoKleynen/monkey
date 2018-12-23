ca5b39f636d4341710178bd09e5406567c04241d
Bo Kleynen <kleynenbo@gmail.com>
2018-12-23 22:51:47 +0100

    Go fmt

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -183,9 +183,9 @@ func TestConditionals(t *testing.T) {
 				code.Make(code.OpTrue),              // 0000
 				code.Make(code.OpJumpNotTruthy, 10), // 0001
 				code.Make(code.OpConstant, 0),       // 0004
-				code.Make(code.OpJump, 11),			 // 0007
+				code.Make(code.OpJump, 11),          // 0007
 				code.Make(code.OpNull),              // 0010
-				code.Make(code.OpPop),				 // 0011
+				code.Make(code.OpPop),               // 0011
 				code.Make(code.OpConstant, 1),       // 0012
 				code.Make(code.OpPop),               // 0015
 			},
