6ac1c427ba98fa16c06bb89d774d1a6bac15f489
Bo Kleynen <kleynenbo@gmail.com>
2019-01-31 13:20:57 +0100

    Compiling function calls

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -247,6 +247,14 @@ func (c *Compiler) Compile(node ast.Node) error {
 		compiledFn := &object.CompiledFunction{Instructions: instructions}
 		c.emit(code.OpConstant, c.addConstant(compiledFn))
 
+	case *ast.CallExpression:
+		err := c.Compile(node.Function)
+		if err != nil {
+			return err
+		}
+
+		c.emit(code.OpCall)
+
 	case *ast.IndexExpression:
 		err := c.Compile(node.Left)
 		if err != nil {
